4668214571734ea112b0f8608d71967148fffd40
jjty <jye.g96@hotmail.com>
2022-04-23 05:11:04 +1000

    Initial Commit

---
+++ twitter/twitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterApp.swift
+//  twitter
+//
+//  Created by jye on 23/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct twitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
